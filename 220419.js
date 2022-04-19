6217fc44ee2897bba937a5e322fb382714641955
choigicheol <kofpo900916@gmail.com>
2022-04-19 15:49:30 +0900

    220419 programmers LV1

--- 220419.js
+++ 220419.js
@@ -50,7 +50,6 @@
 // 입출력 예 #3
 
 // 오른손잡이가 [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]를 순서대로 누르면 사용한 손은 "LLRLLRLLRL"이 됩니다.
-
 function solution(numbers, hand) {
   const keyPad = {
     1: 0,
@@ -74,31 +73,26 @@ function solution(numbers, hand) {
   let result = numbers.map((num) => {
     const target = keyPad[num];
     if (num === 1 || num === 4 || num === 7) {
-      fingerL = [target, 0];
+      fingerL[0] = target;
+      fingerL[1] = 0;
       return "L";
     } else if (num === 3 || num === 6 || num === 9) {
-      fingerR = [target, 2];
+      fingerR[0] = target;
+      fingerR[1] = 2;
       return "R";
     } else {
       // 2,5,8,0 눌렀을때 현재 왼손가락과 오른손가락 위치까지의 거리를 구한다.
-      const distanceR =
-        Math.abs(fingerR[0] - target) + Math.abs(fingerR[1] - 1);
-      const distanceL =
-        Math.abs(fingerL[0] - target) + Math.abs(fingerL[1] - 1);
-      if (distanceR < distanceL) {
-        fingerR = [target, 1];
+      const distanceR = Math.abs(fingerR[0] - target) + Math.abs(fingerR[1] - 1);
+      const distanceL = Math.abs(fingerL[0] - target) + Math.abs(fingerL[1] - 1);
+
+      if (distanceR < distanceL || (distanceR === distanceL && hand === "right")) {
+        fingerR[0] = target;
+        fingerR[1] = 1;
         return "R";
-      } else if (distanceR > distanceL) {
-        fingerL = [target, 1];
-        return "L";
       } else {
-        if (hand === "right") {
-          fingerR = [target, 1];
-          return "R";
-        } else {
-          fingerL = [target, 1];
-          return "L";
-        }
+        fingerL[0] = target;
+        fingerL[1] = 1;
+        return "L";
       }
     }
   });
